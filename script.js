eba8d16821e79dc321bd490ef3e5b7131d3e2087
Mohamad Eko Prasetyo <56286101+meprasetyo@users.noreply.github.com>
2025-04-24 12:57:50 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+
+  <script>
+    document.getElementById("pesanForm").addEventListener("submit", function(e) {
+      e.preventDefault();
+      const formData = new FormData(this);
+
+      fetch("https://script.google.com/macros/s/AKfycbyP8cOQhUFrwZGTGrKsQfc0W8onL4JXhAY_23dLm9MD34DAFMV-5QfVDhOdPA_o-DDa/exec", {
+        method: "POST",
+        body: formData
+      })
+      .then(res => res.text())
+      .then(txt => {
+        alert("Pesan berhasil dikirim!");
+        document.getElementById("pesanForm").reset();
+      })
+      .catch(err => {
+        alert("Gagal mengirim pesan.");
+        console.error(err);
+      });
+    });
+  </script>
